d9379f3cb14653296af3c48c0de76d2b4742f2a4
gavin-yyj <1302330290qq.com>
2020-05-09 16:09:54 +0800

    通过flyway来管理数据库版本(存在bug)

---
+++ flyway/src/test/java/com/yyj/flywaydb/FlywaydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyj.flywaydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaydbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
