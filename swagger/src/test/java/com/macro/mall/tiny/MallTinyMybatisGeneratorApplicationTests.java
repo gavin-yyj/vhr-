20aa3c3092e8eba6b99bd97421fc316bdf7be44b
gavin-yyj <1302330290qq.com>
2020-05-10 11:34:46 +0800

    实现generator与swagger的整合

---
+++ swagger/src/test/java/com/macro/mall/tiny/MallTinyMybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTinyMybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
